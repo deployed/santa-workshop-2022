20e4fa3c01353aef58802652058d8e31c6a0cf5a
Karolina Zielonka <karolina.zielonka@deployed.pl>
2022-12-17 16:37:03 +0100

    distribution view

--- src/paths/pages/Distribution.tsx
+++ src/paths/pages/Distribution.tsx
@@ -1,19 +1,36 @@
 import Topbar from "../../components/Topbar";
 import Box from "@mui/system/Box";
 import PackagesList from "../../components/PackagesList";
+import ProgressBar from "../../components/ProgressBar";
+import Timer from "../../components/Timer";
+import { Grid } from "@mui/material";
 
 const Distribution = () => {
   return (
-    <>
-      <Topbar>Distribution Topbar</Topbar>
-      <Box component="main" sx={{
+    <Box
+      component="main"
+      sx={{
         backgroundColor: "#E7DCD5",
-        flexGrow: 1
-      }}>
-        <PackagesList/>
-      </Box>
-    </>
-  )
-}
+        flexGrow: 1,
+      }}
+    >
+      <Grid
+        container
+        spacing={2}
+        sx={{ margin: "16px", backgroundColor: "#E7DCD5" }}
+      >
+        <Grid item xs={6}>
+          <ProgressBar />
+        </Grid>
+        <Grid item xs={6}>
+          <Timer />
+        </Grid>
+        <Grid item xs={5}>
+          <PackagesList />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-export default Distribution
+export default Distribution;
