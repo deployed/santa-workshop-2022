bc41652f6dab84f806fdd14e7fa987ad83432b5d
dzlk17 <juliaseweryn17@gmail.com>
2022-12-17 16:02:14 +0100

    9

--- src/components/TablePackages/PackageTableRow.tsx
+++ src/components/TablePackages/PackageTableRow.tsx
@@ -1,15 +1,21 @@
 import { CheckBoxOutlineBlankOutlined } from "@mui/icons-material";
 import { TableCell, TableRow } from "@mui/material";
+import PackagingStatus from "../PackagingStatus";
 import { Packages } from "./types";
 import CreationDate from "../CreationDate";
 
-const PackageTableRow = ({ id, kindness,createdAt,  ...rest }: Packages) => {
+const PackageTableRow = ({ id, kindness, createdAt, status, ...rest }: Packages) => {
   return (
     <TableRow>
       <TableCell>
         <CheckBoxOutlineBlankOutlined />
       </TableCell>
       <CreationDate date={createdAt}/>
+      <TableCell></TableCell>
+      <TableCell></TableCell>
+      <TableCell>
+      <PackagingStatus status={status}/>
+      </TableCell>
     </TableRow>
   );
 };
