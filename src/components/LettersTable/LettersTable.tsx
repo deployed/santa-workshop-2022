ab501945d7cabad5b643425a8401fdbc193bca47
jarkol01 <s221485@student.uek.krakow.pl>
2022-12-17 14:47:20 +0100

    poprawione jedno tak elo jeszcze troche

--- src/components/LettersTable/LettersTable.tsx
+++ src/components/LettersTable/LettersTable.tsx
@@ -7,7 +7,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
-import TextInLetterTable from '../TextInLetterTable';
 
 interface TableHeaderElementProps {
     title: string
