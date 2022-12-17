41947ac1a38016e41b5912d90bc54165497627fa
karol.kosek <karol99kos@gmail.com>
2022-12-17 16:13:41 +0100

    Quick fix

--- src/components/VerifyLetterModal/VerifyLetterModal.tsx
+++ src/components/VerifyLetterModal/VerifyLetterModal.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { useState } from 'react';
 
 import LeftSide from './LeftSide';
 import RightSide from './RightSide';
