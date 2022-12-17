4a56d34f674cfcd511bb37ce6581f78114247dbc
Inevera11 <zuzannabozek2002@gmail.com>
2022-12-17 11:55:28 +0100

    create_page_table_component

--- src/components/PackagesTable/PackagesTable.tsx
+++ src/components/PackagesTable/PackagesTable.tsx
@@ -1,9 +1,82 @@
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Checkbox,
+} from "@mui/material";
+import { TableContainer } from "@mui/material";
+import "./PackagesTable.css";
 
-type Props = {};
+function createData(
+  id: number,
+  name: string,
+  status: string,
+  country: string,
+  city: string,
+  created_at: string
+) {
+  return { id, name, status, country, city, created_at };
+}
 
-const packagesTable = (props: Props) => {
-  return <div>packagesTable</div>;
+const rows = [
+  createData(0, "Marek", "new", "Polska", "Kraków", "10-12-2022"),
+  createData(1, "Zosia", "new", "Polska", "Kraków", "10-12-2022"),
+];
+
+const packagesTable = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <h1 className="header">Pakowanie</h1>
+      <TableContainer sx={{ justifyContent: "center", display: "flex" }}>
+        <Table
+          sx={{
+            width: "90%",
+            margin: "1rem",
+            alignSelf: "center",
+            background: "#ffff",
+            boxShadow: "inset 0px -1px 0px rgba(0, 0, 0, 0.12)",
+          }}
+          aria-label="simple table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Checkbox disabled checked />
+              </TableCell>
+              <TableCell align="center">ID listu</TableCell>
+              <TableCell align="center">Imię i Nazwisko</TableCell>
+              <TableCell align="center">Status pakowania</TableCell>
+              <TableCell align="center">Kraj</TableCell>
+              <TableCell align="center">Miasto</TableCell>
+              <TableCell align="center">Data akceptacji</TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => {
+              return (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    <Checkbox />
+                  </TableCell>
+                  <TableCell align="center">{row.id}</TableCell>
+                  <TableCell align="center">{row.name}</TableCell>
+                  <TableCell align="center">{row.status}</TableCell>
+                  <TableCell align="center">{row.country}</TableCell>
+                  <TableCell align="center">{row.city}</TableCell>
+                  <TableCell align="center">{row.created_at}</TableCell>
+                  <TableCell align="center">pakuj</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default packagesTable;
