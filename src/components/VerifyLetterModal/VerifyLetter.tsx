fbd706a1100f1c76c536b468e8d6ce1645ba41cf
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2022-12-17 14:53:05 +0100

    Update VerifyLetter.tsx

--- src/components/VerifyLetterModal/VerifyLetter.tsx
+++ src/components/VerifyLetterModal/VerifyLetter.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Button, Card, CardContent, Grid, makeStyles, Modal, Paper, styled, Typography } from "@mui/material";
+import StarIcon from '@mui/icons-material/Star';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 export default function () {
@@ -34,10 +36,10 @@ export default function () {
         >
             <Box sx={style}>
                 <Grid container spacing={0}>
-                    <Grid item xs={3} sx={{ marginRight: 0 }} >
-                        <Box sx={{ bgcolor: "#F3F7F9", margin: "0px", padding: "0px" }}>
-                            <img src="https://s3-alpha-sig.figma.com/img/66c9/766b/c207d69c2b105689d11455c446de3096?Expires=1672012800&Signature=G-bz4llIrnYsmX7~YJnA3JZA8k8Q7iLhFUYmZbQwzpQfof4hejgLIAabKy1d4ranhvKZbrTKCbacGmz179yYdJ2Yp6y25tjYLSyyaiDCfGXDkS8cGjQCgzKDJJy78nHQjBTwfEtXKi7~MX1GY2mH09fltna7UIMFQsMWym7RyG5SUTnOHkfiSCRVMo1qvs0BgXWiBcj7TRu0CQ7xgjdYTI-hcMUSfuqWu7HbmtwPh0hGmtME23Io1ehvQTp99J9GfrJPPB9xsjPNerJKApJG68LmHQEae3iXBXvmfnwgKA8GbMkQy8o0vNcRQ5Q~4Ctd5RhyVwGCUSDIUy1u9O0L3w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" width="100%" />
-                            <Typography>
+                    <Grid item xs={4}>
+                        <Box sx={{ bgcolor: "#F3F7F9", borderTopLeftRadius: 12, borderBottomLeftRadius: 12 }}>
+                            <img style={{ borderTopLeftRadius: 12 }} src="https://s3-alpha-sig.figma.com/img/66c9/766b/c207d69c2b105689d11455c446de3096?Expires=1672012800&Signature=G-bz4llIrnYsmX7~YJnA3JZA8k8Q7iLhFUYmZbQwzpQfof4hejgLIAabKy1d4ranhvKZbrTKCbacGmz179yYdJ2Yp6y25tjYLSyyaiDCfGXDkS8cGjQCgzKDJJy78nHQjBTwfEtXKi7~MX1GY2mH09fltna7UIMFQsMWym7RyG5SUTnOHkfiSCRVMo1qvs0BgXWiBcj7TRu0CQ7xgjdYTI-hcMUSfuqWu7HbmtwPh0hGmtME23Io1ehvQTp99J9GfrJPPB9xsjPNerJKApJG68LmHQEae3iXBXvmfnwgKA8GbMkQy8o0vNcRQ5Q~4Ctd5RhyVwGCUSDIUy1u9O0L3w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" width="100%" />
+                            <Typography sx={{ padding: 3 }}>
                                 <b>Imię:</b><br />
                                 x<br />
                                 <b>Nazwisko:</b><br />
@@ -55,14 +57,39 @@ export default function () {
                             </Typography>
                         </Box>
                     </Grid>
-                    <Grid item xs={9} sx={{ marginLeft: 0 }}>
-                        <Box sx={{ bgcolor: "white" }}>
-                            Imię: xxx xxxxxxxx<br />
-                            Wiek: x lat<br />
-                            Grzeczność: x<br />
-                            Kraj: x<br />
-                            Miasto: x<br />
-                            Ulica: x<br />
+                    <Grid item xs={8}>
+                        <Box sx={{ borderTopRightRadius: 12, padding: 3 }}>
+                            <Box>
+                                <Grid container spacing={0}>
+                                    <Grid item xs={6}>
+                                        <Typography sx={{ color: "#A9A9A9", width: "50%" }}>
+                                            ID x<br />
+                                            calendar  xx.xx.xxxx<br />
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid item xs={5}>
+                                        <Typography sx={{ borderRadius: 12, border: "solid", width: 100, textAlign: "center" }}>
+                                            category
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid item xs={1}>
+                                        <Button variant="text" sx={{ color: "black" }} onClick={handleClose}>
+                                            <CloseIcon />
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                            <Box sx={{ margin: 3 }}>
+                                <Typography sx={{ fontSize: 25 }}>
+                                    Lista:<br />
+
+                                    <StarIcon sx={{ color: "gray", marginRight: 3, marginLeft: 3 }} />Piłka<br />
+                                    <Button variant="contained" sx={{ margin: 3, marginLeft: 0, borderRadius: 50, width: 200 }}>AKCEPTUJ I WYŚLIJ</Button>
+                                    <Button variant="outlined" sx={{ margin: 3, marginLeft: 0, marginTop: 0, borderRadius: 50, width: 200 }}>ODRZUĆ I DAJ RÓZGĘ</Button>
+                                </Typography>
+                            </Box>
                         </Box>
                     </Grid>
                 </Grid>
