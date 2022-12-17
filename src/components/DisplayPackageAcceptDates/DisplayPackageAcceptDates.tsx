ae06ca47615fb9c950af4729f6fec736f02c0ec3
Michal Zabinski <zabinski@student.agh.edu.pl>
2022-12-17 13:05:44 +0100

    zwracaTableCell

--- src/components/DisplayPackageAcceptDates/DisplayPackageAcceptDates.tsx
+++ src/components/DisplayPackageAcceptDates/DisplayPackageAcceptDates.tsx
@@ -1,19 +1,50 @@
 import React from 'react'
 
-const header = 'Data akceptacji'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { stringify } from 'querystring';
+
+function createData(
+    date: string,
+
+  ) {
+    return {date};
+  }
 
 
 
-const DisplayPackageAcceptDates: React.FC<{dates: string[]} > = ({dates}) => {
-    const oneDate = dates.map((date) => {return <p>{date}</p>})
+const DisplayPackageAcceptDates: React.FC<{date: string} > = ({date}) => {
+    const properDate = formatDate(date);
     
     return(
-        <div>
-            {oneDate}
-        </div>
+        
+        <TableCell component="th" scope="row">
+            {properDate}
+        </TableCell>
+    
 
-    )
+    );
 }
+
+
+function formatDate(baseDate: string): string{
+
+    const newDate = new Date(baseDate);
+
+    const year = newDate.getFullYear();
+    const month = newDate.getMonth();
+    const day = newDate.getDay();
+
+    return day + "-" +  month + "-" + year;
+    
+}
+
+
 
 
 
