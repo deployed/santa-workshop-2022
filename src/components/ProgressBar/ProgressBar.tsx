40e7f29d831dd0da7d592355014538794118c0ba
AgataS23 <agata.stoch@gmail.com>
2022-12-17 15:28:22 +0100

    Added styles.tsx

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,26 +1,12 @@
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
+import { BorderLinearProgress } from './styles';
 
 
 const mockedDeliveriesNumber = 14;
 const max = 12;
 const repeat = Math.floor(mockedDeliveriesNumber/max);
 const percentageDeliveriesNumber = Math.floor((mockedDeliveriesNumber*100)/max) - repeat*100;
-
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-  height: 10,
-  borderRadius: 5,
-  [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-  },
-  [`& .${linearProgressClasses.bar}`]: {
-    borderRadius: 5,
-    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
-  },
-}));
-
 
 
 export default function ProgressBar() {
