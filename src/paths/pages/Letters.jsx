cf09795e1ac9086d95774bf6a5e86b6d8b1a805c
macias95 <116167450+macias95@users.noreply.github.com>
2022-12-17 12:27:07 +0100

    Creat letter table

---
+++ src/paths/pages/Letters.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import Topbar from "../../components/Topbar"
+import Box from "@mui/system/Box"
+import { TableBody, TableCell, TableHead, TableRow } from "@mui/material"
+import TableContainer from "@mui/material/TableContainer"
+import Table from "@mui/material/Table"
+import Paper from "@mui/material/Paper"
+import Checkbox from "@mui/material/Checkbox"
+
+const Letters = () => {
+  const [list, setList] = useState([])
+
+  useEffect(()=> {
+    fetch('https://santa.deployed.space/api/wishlists/')
+  .then((response) => response.json())
+  .then((data) => {setList(data)})
+  },[])
+  
+  return (
+    <>
+      <Topbar>Listy</Topbar>
+      <Box component="main" sx={{
+        backgroundColor: "#E7DCD5",
+        flexGrow: 1
+
+      }}>
+       <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell><Checkbox defaultChecked /></TableCell>
+                <TableCell>Id listu</TableCell>
+                <TableCell>Imię i nazwisko</TableCell>
+                <TableCell>Poziom Grzeczności</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Kraj</TableCell>
+                <TableCell>Miasto</TableCell>
+                <TableCell>Data nadesłania</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+          </TableHead>
+          <TableBody>
+          {list.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>
+                <Checkbox defaultChecked />
+              </TableCell>
+              <TableCell>Przyklad</TableCell>
+            </TableRow>
+          ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      </Box>
+    </>
+  )
+}
+
+export default Letters
