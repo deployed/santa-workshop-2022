16f978427757fb788358e613ae77a818a2bbf9e1
MichaelAngeloTheFirst <siemic13@gmail.com>
2022-12-17 13:13:16 +0100

    Adding table to Packages component

--- src/paths/pages/Packages.tsx
+++ src/paths/pages/Packages.tsx
@@ -1,16 +1,23 @@
-import Box from "@mui/system/Box"
-import Topbar from "../../components/Topbar"
+import Box from "@mui/system/Box";
+import Topbar from "../../components/Topbar";
+import TablePackages from "../../components/TablePackages";
 
 const Packages = () => {
   return (
     <>
       <Topbar>Packages topbar</Topbar>
-      <Box component="main" sx={{
-        backgroundColor: "#E7DCD5",
-        flexGrow: 1
-      }}>Packages</Box>
+      <Box
+        component="main"
+        sx={{
+          padding: "10px",
+          backgroundColor: "#E7DCD5",
+          flexGrow: 1,
+        }}
+      >
+        <TablePackages />
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Packages
+export default Packages;
