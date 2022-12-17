6d7e12259c52bd20404bb581b104d17ca2a332ff
macias95 <116167450+macias95@users.noreply.github.com>
2022-12-17 15:19:43 +0100

    13-component display country

---
+++ src/components/Name/Country.tsx
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography/Typography'
+import React from 'react'
+
+const Country = ({country}: {country : string}) =>{
+    return (<Typography>{country}</Typography>)
+}
+
+export default Country;
